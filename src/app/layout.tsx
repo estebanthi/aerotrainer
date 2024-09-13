7b285c022956091e505ac12e0bb90985e75a985c
estebanthi <esteban.thilliez@gmail.com>
2024-09-13 15:29:18 +0200

    Disconnect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Link from "next/link";
 import {Suspense} from "react";
-import { signOut } from "@/auth";
-import {NextAuthProvider} from "@/app/NextAuthProvider";
+import {auth, signOut} from "@/auth";
 import AuthWrapper from "@/app/auth_wrapper";
 
 const geistSans = localFont({
@@ -23,11 +22,13 @@ export const metadata: Metadata = {
   description: "Un site pour préparer les examens aéronautiques",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+    const session = await auth();
 
   return (
     <html lang="en">
@@ -39,43 +40,22 @@ export default function RootLayout({
         <div className="flex items-center">
             <h1 className="text-2xl font-bold text-gray-900 dark:text-blueish-100">Aerotrainer</h1>
         </div>
-        <nav className="space-x-4">
+        <nav className="space-x-4 flex items-center">
             <Link href="/">
                 <span className="text-gray-700 hover:text-blue-500 transition-colors dark:text-blueish-100">
                     Accueil
                 </span>
             </Link>
-            <form
+            {session && <form
                 action={async () => {
                     'use server';
                     await signOut();
                 }}
             >
-                <button
-                    className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-                    <svg
-                        className="w-6 h-6 text-gray-700 dark:text-blueish-100"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                        ></path>
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                        ></path>
-                    </svg>
-                    <div className="hidden md:block">Sign Out</div>
+                <button className="text-gray-700 hover:text-blue-500 transition-colors dark:text-blueish-100" type="submit">
+                    Déconnexion
                 </button>
-            </form>
+            </form>}
         </nav>
     </header>
     <Suspense fallback={<div>Chargement...</div>}>{children}</Suspense>
